d80805bff75a38ed4ddcde7fe091930aa4d7911e
DS-Amaya <62181202+DS-Amaya@users.noreply.github.com>
2020-05-31 17:20:33 -0500

    Add files via upload

--- Proyectos/Segundo Corte/Colisiones 2D/Esferas2.cs
+++ Proyectos/Segundo Corte/Colisiones 2D/Esferas2.cs
@@ -2,20 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Esfera : MonoBehaviour
+public class Esferas2 : MonoBehaviour
 {
     public Vector3 V;
     Vector3 P;
     float tiempo = 0.01f;
     Color Color;
     public float Radio = 0.6f;
-    float ancho, alto;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        ancho = 10f;
-        alto = 10f;
+        Time.fixedDeltaTime = 0.01f;
+        tiempo = Time.fixedDeltaTime;
         Color.r = Random.Range(0f, 1f);
         Color.g = Random.Range(0f, 1f);
         Color.b = Random.Range(0f, 1f);
@@ -24,31 +24,10 @@ public class Esfera : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         P = gameObject.GetComponent<Transform>().position;
         P = P + V * tiempo;
         gameObject.GetComponent<Transform>().position = P;
-
-       //Pared derecha
-        if ((P.x + Radio) >= ancho)
-        {
-            V.x = -V.x;
-        }
-        //Pared izda
-        if ((P.x - Radio) <= -ancho)
-        {
-            V.x = -V.x;
-        }
-        //Pared arriba
-        if ((P.y + Radio) >= alto)
-        {
-            V.y = -V.y;
-        }
-        //Pared abajo
-        if ((P.y - Radio) <= -alto)
-        {
-            V.y = -V.y;
-        }
     }
-}
+ }
